93d7dcadd86220eca59e946ad22874d85003b321
TyYan03 <tylery202@gmail.com>
2023-11-04 21:57:07 -0400

    more code

--- testPy/app.py
+++ testPy/app.py
@@ -1,10 +1,8 @@
 # print("test")
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 import subprocess
 
 app = Flask(__name__)
-
-global process
 
 @app.route('/')
 def index():
@@ -17,6 +15,23 @@ def learn():
     # Execute the Python script using subprocess
     #subprocess.run(['python', script_path], capture_output=False, text=False, check=False)
     return render_template('learn.html')
+
+@app.route('/start_script', methods=['POST'])
+def start_script():
+        global process
+    #if process is None or process.returncode is not None:
+        process = subprocess.Popen(['python', './inference_classifier.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return jsonify(success=True)
+        return jsonify(success=False)
+
+@app.route('/stop_script', methods=['POST'])
+def stop_script():
+    #global process
+        global process
+    #if process and process.returncode is None:
+        process.terminate()
+        return jsonify(success=True)
+    #return jsonify(success=False)
 
 @app.route('/startFun', methods=['POST'])
 def startFun():
