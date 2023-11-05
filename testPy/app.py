d0e41c2b7e563405e385ae35459a79e6413d147c
maanikg <gognamaanik@gmail.com>
2023-11-04 22:47:41 -0400

    adding about page

--- testPy/app.py
+++ testPy/app.py
@@ -4,53 +4,79 @@ import subprocess
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/learn')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/aboutTest")
+def aboutTest():
+    return render_template("aboutTest.html")
+
+
+@app.route("/learn")
 def learn():
-    #script_path = './inference_classifier.py'
-    
+    # script_path = './inference_classifier.py'
+
     # Execute the Python script using subprocess
-    #subprocess.run(['python', script_path], capture_output=False, text=False, check=False)
-    return render_template('learn.html')
+    # subprocess.run(['python', script_path], capture_output=False, text=False, check=False)
+    return render_template("learn.html")
 
-@app.route('/start_script', methods=['POST'])
+
+@app.route("/start_script", methods=["POST"])
 def start_script():
-        global process
-    #if process is None or process.returncode is not None:
-        process = subprocess.Popen(['python', './inference_classifier.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        return jsonify(success=True)
-        return jsonify(success=False)
+    global process
+    # if process is None or process.returncode is not None:
+    process = subprocess.Popen(
+        ["python", "./inference_classifier.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+    )
+    return jsonify(success=True)
+    return jsonify(success=False)
 
-@app.route('/stop_script', methods=['POST'])
+
+@app.route("/stop_script", methods=["POST"])
 def stop_script():
-    #global process
-        global process
-    #if process and process.returncode is None:
-        process.terminate()
-        return jsonify(success=True)
-    #return jsonify(success=False)
-
-@app.route('/startFun', methods=['POST'])
-def startFun():
-    process = subprocess.Popen(['python', './inference_classifier.py'])
-    return redirect(url_for('/learn'))
-
-@app.route('/stopFun', methods=['POST'])
-def stopFun():
+    # global process
+    global process
+    # if process and process.returncode is None:
     process.terminate()
-    #return redirect(url_for('index'))
+    return jsonify(success=True)
 
 
-@app.route('/run-script')
+# return jsonify(success=False)
+
+
+# @app.route("/startFun", methods=["POST"])
+# def startFun():
+#     process = subprocess.Popen(["python", "./inference_classifier.py"])
+#     return redirect(url_for("/learn"))
+
+
+# @app.route("/stopFun", methods=["POST"])
+# def stopFun():
+#     process.terminate()
+# return redirect(url_for('index'))
+
+
+@app.route("/run-script")
 def run_script():
     # Replace 'your_script.py' with the path to your Python script
-    script_path = './inference_classifier.py'
-    
-    # Execute the Python script using subprocess
-    subprocess.run(['python', script_path], capture_output=False, text=False, check=False)
+    script_path = "./inference_classifier.py"
 
-if __name__ == '__main__':
+    # Execute the Python script using subprocess
+    subprocess.run(
+        ["python", script_path], capture_output=False, text=False, check=False
+    )
+
+
+if __name__ == "__main__":
     app.run(debug=True)
